befbd64ecc6ec994f798cca21ee8a4234465959f
sai <saiforceone@gmail.com>
2024-12-27 16:43:48 -0600

    implement request and application state cache

--- fstack-backend-api/app.py
+++ fstack-backend-api/app.py
@@ -1,12 +1,16 @@
 # File: app.py
 # Desc: The entrypoint of this tutorial application
+from contextlib import asynccontextmanager
+
 from starlette.applications import Starlette
 from starlette.middleware import Middleware
 from starlette.middleware.cors import CORSMiddleware
 from starlette.responses import JSONResponse
 from starlette.routing import Route
 
+from models.feature_flag import FeatureFlag
 from routes import app_routes
+from support.db_utils import db_utils
 
 
 async def main(request):
@@ -23,4 +27,21 @@ middleware = [
     Middleware(CORSMiddleware, allow_origins=['*'], allow_methods=['*'], allow_headers=['*']),
 ]
 
-app = Starlette(debug=True, middleware=middleware, routes=routes)
+
+@asynccontextmanager
+async def lifespan(application):
+    """
+    This method performs tasks for application startup and shutdown. In this case, we are caching the enabled feature
+    flags on application startup, if none are found, we'll set the cached flags to an empty list
+    :param application:
+    :return:
+    """
+    try:
+        enabled_features = db_utils.session.query(FeatureFlag).where(FeatureFlag.enabled).all()
+        setattr(application.state, 'CACHED_FLAGS', [flag.to_dict() for flag in enabled_features])
+    except Exception as e:
+        print(f"failed to retrieve enabled feature flags with error: {e}. Continuing application startup...")
+        setattr(application.state, 'CACHED_FLAGS', [])
+    yield
+
+app = Starlette(debug=True, middleware=middleware, routes=routes, lifespan=lifespan)
--- fstack-backend-api/support/require_feature_flag.py
+++ fstack-backend-api/support/require_feature_flag.py
@@ -5,7 +5,8 @@ from starlette.requests import Request
 from starlette.responses import JSONResponse
 
 from models.feature_flag import FeatureFlag
-from .db_utils import db_utils
+from support.db_utils import db_utils
+
 
 def require_feature_flag(required_flag: str):
 
@@ -21,21 +22,36 @@ def require_feature_flag(required_flag: str):
         
         @functools.wraps(func)
         async def exec_feature_check(*args, **kwargs):
+            request = kwargs.get("request", args[idx] if idx < len(args) else None)
+            assert isinstance(request, Request)
 
+            # retrieve feature flags from the application state
+            cached_flags = getattr(request.app.state, 'CACHED_FLAGS', [])
 
-            # try to retrieve feature flags matching the required_flag            
-            try:
-                feature_flag = db_utils.session.query(
-                    FeatureFlag
-                ).where(
-                    FeatureFlag.flag == required_flag, FeatureFlag.enabled == True
-                ).scalar()
-            except Exception as e:
-                print(f"failed to retrieve feature flag with error: {e}")
-                return JSONResponse({
-                    'success': False,
-                    'message': 'Unexpected error occurred'
-                }, status_code=500)
+            feature_flag = None
+
+            # check the list of cached_flags
+            if len(cached_flags) > 0:
+                print("cache hit!")
+                # search the cache
+                for flag in cached_flags:
+                    if flag['flag'] == required_flag:
+                        feature_flag = flag
+            else:
+                print("cache miss")
+                # try to retrieve feature flags matching the required_flag
+                try:
+                    feature_flag = db_utils.session.query(
+                        FeatureFlag
+                    ).where(
+                        FeatureFlag.flag == required_flag, FeatureFlag.enabled == True
+                    ).scalar()
+                except Exception as e:
+                    print(f"failed to retrieve feature flag with error: {e}")
+                    return JSONResponse({
+                        'success': False,
+                        'message': 'Unexpected error occurred'
+                    }, status_code=500)
             
             if feature_flag is None:
                 print(f"the feature: {required_flag} was not found or is invalid")
